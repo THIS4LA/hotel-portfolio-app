df343ec7d3563e3aae507f41f67df4262ccf8d1b
Ravindu Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2024-04-25 18:25:49 +0530

    added home page with bgvideo(didn't work)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/header/header';
+import Home from './pages/home';
 
 function App() {
   return (
     <div>
       <Header />
+      <Home />
     </div>
   );
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import mutedVideo from './../assets/video1.mp4'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="flex justify-center items-center h-screen">
+      {/* VIDEO */}
+      <div className="relative w-full aspect-w-16 aspect-h-9">
+        <video autoPlay muted loop className="absolute inset-0 w-full h-full object-cover">
+          <source src={mutedVideo} alt='video'/>
+        </video>
+      </div>
+      
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
