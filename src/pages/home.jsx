92602e4573324ad844c30cd8e92f36b0d5679fb8
Ravindu Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2024-04-26 08:55:02 +0530

    unnessary

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import mutedVideo from './../assets/video1.mp4'
+import React from "react";
+import mutedVideo from "./../assets/video1.mp4";
 
 const Home = () => {
   return (
     <div className="flex justify-center items-center h-screen">
       {/* VIDEO */}
       <div className="relative w-full aspect-w-16 aspect-h-9">
-        <video autoPlay muted loop className="absolute inset-0 w-full h-full object-cover">
-          <source src={mutedVideo} alt='video'/>
+        <video
+          autoPlay
+          muted
+          loop
+          className="absolute inset-0 w-full h-full object-cover"
+        >
+          <source src={mutedVideo} alt="video" />
         </video>
       </div>
-      
     </div>
   );
-}
+};
 
 export default Home;
