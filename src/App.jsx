de5da3b7a44145a86af28648bc5d49d5504ee054
Ravindu Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2024-04-27 15:46:36 +0530

    aded footer,signin,card templates

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/header/header';
+import Card from './pages/card';
 import Home from './pages/home';
 
 function App() {
   return (
     <div>
       <Header />
+      <Card />
       <Home />
     </div>
   );
